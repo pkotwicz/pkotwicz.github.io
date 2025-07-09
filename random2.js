965cd74a7eb7a1dae4d9f3e308c957191dd7be0c
pkotwicz <pkotwicz@chromium.org>
2025-07-09 18:37:06 -0400

    Update random2.js

--- random2.js
+++ random2.js
@@ -61,7 +61,35 @@ async function requestIssuance(requestedProtocol, doctype, requestedMdocAttribut
 }
 
 async function requestDigitalCreds(requestedProtocol, doctype, requestedMdocAttributes, transactionData = null, issuanceOffer = null, vct_values = null, encrypt_response = false, sign_request = false, dcql_query = null) {
-    var request = await getRequest(requestedProtocol, doctype, requestedMdocAttributes, transactionData, issuanceOffer, vct_values, encrypt_response, sign_request, dcql_query)
+        var request = {
+            protocol:"openid4vp",
+            request:{
+                dcql_query:{
+                    "credentials":[
+                        {"claims":[
+                            {"path":["org.iso.18013.5.1","family_name"]},
+                            {"path":["org.iso.18013.5.1","given_name"]},
+                            {"path":["org.iso.18013.5.1","age_over_21"]}
+                        ],
+                         "format":"mso_mdoc",
+                         "id":"cred1",
+                         "meta":{
+                             "doctype_value":"org.iso.18013.5.1.mDL"
+                         }
+                        }
+                    ]
+                },
+                "nonce":"Jqxt2X1bnuVx1eKa3MjV5QX4T7UVbhPewx76ZkaGT7Q",
+                "response_mode":"dc_api",
+                "response_type":"vp_token"
+            },
+            "state":{
+                "credential_type":"mso_mdoc",
+                "nonce":"Jqxt2X1bnuVx1eKa3MjV5QX4T7UVbhPewx76ZkaGT7Q",
+                "private_key":"ZBBagBjKmy7XWf3abOn7b9JgVgbHTQ2bBtPaUB2tuXY=",
+                "public_key":"BH9BqyuIptTbBq2EJaOvaVhfPlXkRldkJjJaZFeHi5lGFl_ZMt5wfv8sGTxhDNs1LBoUmSNyjD3WDp5IPMsM4zg="
+            }
+        };
     try {
         var abortController = new AbortController();
         var credentialResponse  = await navigator.credentials.get({
